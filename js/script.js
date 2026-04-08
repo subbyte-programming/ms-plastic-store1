623c008d1969d4a70a035967e6cf28dbb82721ff
AB SUBHAN <128168821+ABSUBHAN1@users.noreply.github.com>
2026-04-08 20:29:53 +0500

    Initial commit - MS Plastic Store Website

---
+++ js/script.js
@@ -0,0 +1,132 @@
+(function($) {
+
+  "use strict";
+
+  var initPreloader = function() {
+    $(document).ready(function($) {
+      var Body = $('body');
+      Body.addClass('preloader-site');
+    });
+    $(window).on('load', function() {
+      $('.preloader-wrapper').fadeOut();
+      $('body').removeClass('preloader-site');
+    });
+  }
+
+  // init Chocolat light box
+  var initChocolat = function() {
+    Chocolat(document.querySelectorAll('.image-link'), {
+      imageSize: 'contain',
+      loop: true,
+    });
+  }
+
+  var initSwiper = function() {
+
+    var swiper = new Swiper(".main-swiper", {
+      speed: 500,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+    var category_swiper = new Swiper(".category-carousel", {
+      slidesPerView: 8,
+      spaceBetween: 30,
+      speed: 500,
+      navigation: {
+        nextEl: ".category-carousel-next",
+        prevEl: ".category-carousel-prev",
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        991: {
+          slidesPerView: 5,
+        },
+        1500: {
+          slidesPerView: 8,
+        },
+      }
+    });
+
+    $(".products-carousel").each(function(){
+      var $el_id = $(this).attr('id');
+
+      var products_swiper = new Swiper("#"+$el_id+" .swiper", {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        speed: 500,
+        navigation: {
+          nextEl: "#"+$el_id+" .products-carousel-next",
+          prevEl: "#"+$el_id+" .products-carousel-prev",
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          991: {
+            slidesPerView: 4,
+          },
+          1500: {
+            slidesPerView: 5,
+          },
+        }
+      });
+
+    });
+
+    // product single page
+    var thumb_slider = new Swiper(".product-thumbnail-slider", {
+      slidesPerView: 5,
+      spaceBetween: 20,
+      direction: "vertical",
+      breakpoints: {
+        0: {
+          direction: "horizontal"
+        },
+        992: {
+          direction: "vertical"
+        },
+      },
+    });
+
+    var large_slider = new Swiper(".product-large-slider", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      effect: 'fade',
+      thumbs: {
+        swiper: thumb_slider,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+
+  // init jarallax parallax
+  var initJarallax = function() {
+    jarallax(document.querySelectorAll(".jarallax"));
+    jarallax(document.querySelectorAll(".jarallax-keep-img"), {
+      keepImg: true,
+    });
+  }
+
+  // document ready
+  $(document).ready(function() {
+    initPreloader();
+    initSwiper();
+    initJarallax();
+    initChocolat();
+  });
+
+})(jQuery);
